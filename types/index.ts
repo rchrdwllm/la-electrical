a481f8040fe5707118897205f73d26b52bbbe416
Richard William <rwilliam.flores@gmail.com>
2023-11-16 20:00:17 +0800

    added Reservation interface

--- types/index.ts
+++ types/index.ts
@@ -10,4 +10,16 @@ export interface Colors {
     secondaryAccent: string;
     primaryBorder: string;
     primaryShadow: string;
+    success: string;
+    warning: string;
+}
+
+export interface Reservation {
+    createdAt: string;
+    reservationDate: string;
+    name: string;
+    typeOfService: string;
+    id: string;
+    isPaid: boolean;
+    price: string;
 }
