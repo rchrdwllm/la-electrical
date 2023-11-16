23eb3c79a48712b54b66cb537edf548123ec3059
Richard William <rwilliam.flores@gmail.com>
2023-11-16 19:59:49 +0800

    added mock data from mock API

--- components/home/ReservationsSection.tsx
+++ components/home/ReservationsSection.tsx
@@ -2,21 +2,36 @@ import { View, StyleSheet } from 'react-native';
 import Text from '../shared/Text';
 import { Colors } from '../../types';
 import ReservationItem from './ReservationItem';
+import { Link } from 'expo-router';
+import { Reservation } from '../../types';
+import Button from '../shared/Button';
 import { useTheme } from '../../hooks/useTheme';
+import { useEffect, useMemo, useState } from 'react';
+import { fetchReservations } from '../../utils/reservations';
 
 const ReservationsSection = () => {
     const { palette } = useTheme();
     const styles = styling(palette);
+    const [reservations, setReservations] = useState<Reservation[]>([]);
+
+    useEffect(() => {
+        fetchReservations().then(fetchedReservations => {
+            setReservations(fetchedReservations.splice(0, 3));
+        });
+    }, []);
 
     return (
         <View>
             <Text fontWeight="medium">Reservations</Text>
             <View style={styles.reservationsContainer}>
-                <ReservationItem />
-                <ReservationItem />
-                <ReservationItem />
-                <ReservationItem />
-                <ReservationItem />
+                {reservations.map(reservation => (
+                    <ReservationItem key={reservation.id} {...reservation} />
+                ))}
+                <View style={styles.viewAllBtnContainer}>
+                    <Link asChild href="/reservations">
+                        <Button style={styles.viewAllBtn} text="View all" variant="tertiary" />
+                    </Link>
+                </View>
             </View>
         </View>
     );
@@ -36,6 +51,15 @@ const styling = (palette: Colors) =>
             },
             shadowOpacity: 0.3,
             shadowRadius: 9.11,
+        },
+        viewAllBtnContainer: {
+            width: '100%',
+            alignItems: 'flex-end',
+        },
+        viewAllBtn: {
+            paddingVertical: 16,
+            paddingHorizontal: 16,
+            width: 'auto',
         },
     });
 
