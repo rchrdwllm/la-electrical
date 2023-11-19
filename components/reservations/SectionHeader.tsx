6d1a93ec57492af8481291a2692d90a98da4e45c
Richard William <rwilliam.flores@gmail.com>
2023-11-19 16:51:29 +0800

    updated styles

--- components/reservations/SectionHeader.tsx
+++ components/reservations/SectionHeader.tsx
@@ -1,33 +1,20 @@
 import { View, StyleSheet } from 'react-native';
 import Text from '../shared/Text';
-import { Colors } from '../../types';
-import { useTheme } from '../../hooks/useTheme';
 
 const SectionHeader = ({ title }: { title: string }) => {
-    const { palette } = useTheme();
-    const styles = styling(palette);
+    const styles = styling();
 
     return (
         <View style={styles.container}>
-            <View style={styles.textContainer}>
-                <Text>{title}</Text>
-            </View>
+            <Text fontWeight="medium">{title}</Text>
         </View>
     );
 };
 
-const styling = (palette: Colors) =>
+const styling = () =>
     StyleSheet.create({
         container: {
-            width: '100%',
-            position: 'relative',
-        },
-        textContainer: {
-            position: 'absolute',
-            top: 0,
-            left: -16,
-            backgroundColor: palette.primaryBackground,
-            paddingLeft: 16,
+            marginTop: 16,
         },
     });
 
