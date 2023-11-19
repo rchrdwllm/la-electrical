9cdedade82f31363008fcea46a94e662a6f0afda
Richard William <rwilliam.flores@gmail.com>
2023-11-19 16:52:02 +0800

    added caching

--- utils/reservations.ts
+++ utils/reservations.ts
@@ -1,3 +1,4 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Reservation } from '../types';
 
 export const fetchReservations = async () => {
@@ -10,4 +11,46 @@ export const fetchReservations = async () => {
     });
 
     return sortedData;
+};
+
+export const groupByPayment = (reservations: Reservation[]) => {
+    const paidReservations: (string | Reservation)[] = [];
+    const unpaidReservations: (string | Reservation)[] = [];
+
+    reservations.forEach(reservation => {
+        if (reservation.isPaid) {
+            paidReservations.push(reservation);
+        } else {
+            unpaidReservations.push(reservation);
+        }
+    });
+
+    paidReservations.unshift('Paid reservations');
+    unpaidReservations.unshift('Pending payment');
+
+    return [...paidReservations, ...unpaidReservations];
+};
+
+export const storeReservations = async (reservations: Reservation[]) => {
+    try {
+        await AsyncStorage.setItem('reservations', JSON.stringify(reservations));
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const getSavedReservations = async () => {
+    let savedReservations: Reservation[] | null = null;
+
+    try {
+        const reservationsJson = await AsyncStorage.getItem('reservations');
+
+        if (reservationsJson) {
+            savedReservations = JSON.parse(reservationsJson);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
+    return savedReservations;
 };
