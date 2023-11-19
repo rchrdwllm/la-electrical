892c755aacc12064100212fe960cde6fece8c86b
Richard William <rwilliam.flores@gmail.com>
2023-11-19 16:51:55 +0800

    removed extra import

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
