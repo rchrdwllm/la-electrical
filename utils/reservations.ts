88b29776885dd852d84e80b4fb9d85d61a3897f9
Richard William <rwilliam.flores@gmail.com>
2023-11-21 23:13:33 +0800

    added deleting functionality

--- utils/reservations.ts
+++ utils/reservations.ts
@@ -1,7 +1,17 @@
 import 'react-native-get-random-values';
 import { Reservation } from '../types';
 import { Dispatch, SetStateAction } from 'react';
-import { collection, onSnapshot, query, addDoc, Timestamp, FieldValue } from 'firebase/firestore';
+import {
+    collection,
+    onSnapshot,
+    query,
+    addDoc,
+    Timestamp,
+    deleteDoc,
+    where,
+    doc,
+    setDoc,
+} from 'firebase/firestore';
 import { firestore } from '../config/firebase';
 import { v4 as uuid } from 'uuid';
 
@@ -31,15 +41,19 @@ export const addReservation = async (data: {
     isPaid: boolean;
     price: number;
 }) => {
-    const reservationsCollection = collection(firestore, 'reservations');
-
     const reservation = {
         ...data,
         reservationDate: Timestamp.fromDate(data.reservationDate),
         id: uuid(),
     };
 
-    await addDoc(reservationsCollection, reservation);
+    await setDoc(doc(firestore, 'reservations', reservation.id), {
+        ...reservation,
+    });
+};
+
+export const deleteReservation = async (id: string) => {
+    await deleteDoc(doc(firestore, 'reservations', id));
 };
 
 export const groupByPayment = (reservations: Reservation[]) => {
