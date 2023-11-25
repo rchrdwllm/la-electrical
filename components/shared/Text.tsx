06edeb2b7680fcfe72eb945a764d6affe5e0c835
Richard William <rwilliam.flores@gmail.com>
2023-11-25 23:26:51 +0800

    added truncating of long text

--- components/shared/Text.tsx
+++ components/shared/Text.tsx
@@ -15,37 +15,40 @@ interface TextProps extends RNTextProps {
     style?: StyleProp<TextStyle> | StyleProp<TextStyle>[];
 }
 
-const Text = forwardRef(({ children, variant, fontWeight, style }: TextProps, ref) => {
-    let [fontsLoaded, fontError] = useFonts({
-        Lexend_300Light,
-        Lexend_400Regular,
-        Lexend_700Bold,
-    });
-    const { palette } = useTheme();
+const Text = forwardRef(
+    ({ children, variant, fontWeight, style, numberOfLines }: TextProps, ref) => {
+        let [fontsLoaded, fontError] = useFonts({
+            Lexend_300Light,
+            Lexend_400Regular,
+            Lexend_700Bold,
+        });
+        const { palette } = useTheme();
 
-    if (!fontsLoaded && !fontError) {
-        return null;
+        if (!fontsLoaded && !fontError) {
+            return null;
+        }
+
+        return (
+            <RNText
+                style={[
+                    {
+                        color: palette[variant ?? 'primaryText'],
+                        fontFamily:
+                            fontWeight === 'medium'
+                                ? 'Lexend_400Regular'
+                                : fontWeight === 'bold'
+                                ? 'Lexend_700Bold'
+                                : 'Lexend_300Light',
+                    },
+                    style,
+                ]}
+                numberOfLines={numberOfLines}
+            >
+                {children}
+            </RNText>
+        );
     }
-
-    return (
-        <RNText
-            style={[
-                {
-                    color: palette[variant ?? 'primaryText'],
-                    fontFamily:
-                        fontWeight === 'medium'
-                            ? 'Lexend_400Regular'
-                            : fontWeight === 'bold'
-                            ? 'Lexend_700Bold'
-                            : 'Lexend_300Light',
-                },
-                style,
-            ]}
-        >
-            {children}
-        </RNText>
-    );
-});
+);
 
 (Text as any).Reanimated = ({ variant, fontWeight, style, children }: AnimateProps<TextProps>) => {
     const { palette } = useTheme() ?? 'light';
