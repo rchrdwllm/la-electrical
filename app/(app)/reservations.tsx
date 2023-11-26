44af3b49b5002607ea5f29a5284b8aa2b96bd599
Richard William <rwilliam.flores@gmail.com>
2023-11-26 19:58:16 +0800

    added function to edit reservation

--- app/(app)/reservations.tsx
+++ app/(app)/reservations.tsx
@@ -1,15 +1,21 @@
 import { View, StyleSheet } from 'react-native';
 import { Colors } from '../../types';
 import ReservationsList from '../../components/reservations/ReservationsList';
+import EditReservation from '../../components/shared/EditReservation';
+import Reanimated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import { useTheme } from '../../hooks/useTheme';
 import { StatusBar, setStatusBarStyle } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const Reservations = () => {
     const { theme, palette } = useTheme();
     const styles = styling(palette);
     const { top } = useSafeAreaInsets();
+    const [reservationToEdit, setReservationToEdit] = useState<string | null>(null);
+    const scaleValue = useSharedValue(1);
+    const borderRadiusValue = useSharedValue(0);
 
     useEffect(() => {
         setTimeout(() => {
@@ -17,8 +23,27 @@ const Reservations = () => {
         }, 570);
     }, [theme]);
 
+    const animatedScale = useAnimatedStyle(() => ({
+        transform: [
+            {
+                scale: scaleValue.value,
+            },
+        ],
+        borderRadius: borderRadiusValue.value,
+    }));
+
+    useEffect(() => {
+        if (reservationToEdit) {
+            scaleValue.value = withTiming(0.9);
+            borderRadiusValue.value = withTiming(8);
+        } else {
+            scaleValue.value = withTiming(1);
+            borderRadiusValue.value = withTiming(1);
+        }
+    }, [reservationToEdit]);
+
     return (
-        <View style={styles.container}>
+        <GestureHandlerRootView style={styles.parentContainer}>
             <StatusBar animated style={theme === 'light' ? 'dark' : 'light'} />
             <View
                 style={[
@@ -28,24 +53,38 @@ const Reservations = () => {
                     },
                 ]}
             ></View>
-            <ReservationsList />
-        </View>
+            <Reanimated.View style={[styles.container, animatedScale]}>
+                <View style={{ flex: 1 }}>
+                    <ReservationsList setReservationToEdit={setReservationToEdit} />
+                </View>
+            </Reanimated.View>
+            {reservationToEdit && (
+                <EditReservation
+                    reservationToEdit={reservationToEdit}
+                    setReservationToEdit={setReservationToEdit}
+                />
+            )}
+        </GestureHandlerRootView>
     );
 };
 
 const styling = (palette: Colors) =>
     StyleSheet.create({
+        parentContainer: {
+            flex: 1,
+            backgroundColor: palette.black,
+        },
         container: {
             flex: 1,
             backgroundColor: palette.primaryBackground,
             position: 'relative',
+            overflow: 'hidden',
         },
         safeAreaView: {
             position: 'absolute',
             top: 0,
             left: 0,
             width: '100%',
-            backgroundColor: palette.primaryBackground,
             zIndex: 3,
         },
         text: {
