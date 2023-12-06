5e9f894646eedce8cf92ffb4a5b40d8061955c9e
Richard William <rwilliam.flores@gmail.com>
2023-12-06 15:46:35 +0800

    Remove unused code and optimize app initialization

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from 'expo-router';
 import { AuthProvider } from '../context/Auth';
 import SplashScreen from '../components/SplashScreen';
 import Reanimated, { FadeIn, FadeOut } from 'react-native-reanimated';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, LogBox } from 'react-native';
 import { useTheme } from '../hooks/useTheme';
 import { Colors } from '../types';
@@ -14,34 +14,16 @@ const Layout = () => {
     const { palette } = useTheme();
     const styles = styling(palette);
 
-    const prepare = async () => {
-        try {
-            await new Promise(resolve => setTimeout(resolve, 2000));
-        } catch (e) {
-            console.warn(e);
-        } finally {
-            setAppIsReady(true);
-        }
-    };
-
     useEffect(() => {
-        prepare();
+        setTimeout(() => {
+            setAppIsReady(true);
+        });
     }, []);
-
-    const onLayoutRootView = useCallback(async () => {
-        if (appIsReady) {
-        }
-    }, [appIsReady]);
 
     if (!appIsReady) return <SplashScreen />;
 
     return (
-        <Reanimated.View
-            entering={FadeIn.delay(250)}
-            exiting={FadeOut}
-            onLayout={onLayoutRootView}
-            style={styles.container}
-        >
+        <Reanimated.View entering={FadeIn.delay(250)} exiting={FadeOut} style={styles.container}>
             <AuthProvider>
                 <Stack
                     initialRouteName="sign-in"
