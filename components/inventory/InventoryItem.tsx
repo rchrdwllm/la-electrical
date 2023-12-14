02729720da26eee1c68e1899f6c47ba4f6a5665f
Richard William <rwilliam.flores@gmail.com>
2023-12-14 17:53:47 +0800

    Refactor InventoryItem and InventoryList components

--- components/inventory/InventoryItem.tsx
+++ components/inventory/InventoryItem.tsx
@@ -1,29 +1,45 @@
-import { View, StyleSheet } from "react-native";
-import Text from "../shared/Text";
-import { Colors, Inventory } from "../../types";
-import { useTheme } from "../../hooks/useTheme";
+import { View, StyleSheet } from 'react-native';
+import Text from '../shared/Text';
+import { Colors, Inventory } from '../../types';
+import { useTheme } from '../../hooks/useTheme';
 
-const InventoryItem = ({ image, name, number }: Inventory) => {
-  const { palette } = useTheme();
-  const styles = styling(palette);
-  return (
-    <View style={styles.container}>
-      <Text>{name}</Text>
-      <Text>{number}</Text>
-    </View>
-  );
+interface InventoryItemProps extends Inventory {
+    index: number;
+}
+
+const InventoryItem = ({ image, name, number, index }: InventoryItemProps) => {
+    const { palette } = useTheme();
+    const styles = styling(palette);
+
+    return (
+        <View
+            style={[
+                styles.container,
+                index % 2 === 0
+                    ? {
+                          marginRight: 10,
+                      }
+                    : {
+                          marginLeft: 10,
+                      },
+            ]}
+        >
+            <Text>{name}</Text>
+            <Text>{number}</Text>
+        </View>
+    );
 };
 
 const styling = (palette: Colors) =>
-  StyleSheet.create({
-    container: {
-      padding: 20,
-      backgroundColor: palette.secondaryBackground,
-      width: "50%",
-      height: 200,
-      justifyContent: "center",
-      alignItems: "center",
-    },
-  });
+    StyleSheet.create({
+        container: {
+            padding: 20,
+            backgroundColor: palette.secondaryBackground,
+            flex: 1,
+            height: 200,
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+    });
 
 export default InventoryItem;
--- components/inventory/InventoryList.tsx
+++ components/inventory/InventoryList.tsx
@@ -1,20 +1,26 @@
-import { FlatList, View } from "react-native";
-import { inventoryItems } from "../../constants/inventory-items";
-import InventoryItem from "./InventoryItem";
+import { FlatList, View } from 'react-native';
+import { inventoryItems } from '../../constants/inventory-items';
+import InventoryItem from './InventoryItem';
+import { useCallback } from 'react';
+import { Inventory } from '../../types';
 
 const InventoryList = () => {
-  return (
-    <View>
-      <FlatList
-        data={inventoryItems}
-        renderItem={({ item }) => <InventoryItem {...item} />}
-        keyExtractor={(item) => item.name}
-        numColumns={2}
-        key={"_"}
-        contentContainerStyle={{ margin: 16, gap: 16 }}
-      />
-    </View>
-  );
+    const renderItem = useCallback(({ index, item }: { item: Inventory; index: number }) => {
+        return <InventoryItem {...item} index={index} />;
+    }, []);
+
+    return (
+        <View>
+            <FlatList
+                data={inventoryItems}
+                renderItem={renderItem}
+                keyExtractor={item => item.name}
+                numColumns={2}
+                key={'_'}
+                contentContainerStyle={{ gap: 16 }}
+            />
+        </View>
+    );
 };
 
 export default InventoryList;
